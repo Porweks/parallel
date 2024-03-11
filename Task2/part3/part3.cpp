f50dad610389f9e04cb931ffff58c1ed50f86d2b
Porweks <svjatking42@mail.ru>
2024-03-11 14:00:12 +0000

    fixed 3rd part

--- Task2/part3/part3.cpp
+++ Task2/part3/part3.cpp
@@ -2,19 +2,18 @@
 #include <omp.h>
 #include <cmath>
 
-#define N 200
-#define nt 5
-#define variable -0.01
+#define N 46340
+#define nt 80
+#define variable (double)1/N
 
 void matrix_vector_product_omp(double *a, double *b, double *c, int m, int n){
-    #pragma omp parallel num_threads(nt)
+    #pragma omp parallel
     {
         int nthreads = omp_get_num_threads();
         int threadid = omp_get_thread_num();
         int items_per_thread = m / nthreads;
         int lb = threadid * items_per_thread;
         int ub = (threadid == nthreads - 1) ? (m - 1) : (lb + items_per_thread - 1);
-        // #pragma omp for
         for (int i = lb; i <= ub; i++) {
             c[i] = 0.0;
             for (int j = 0; j < n; j++)
@@ -24,76 +23,68 @@ void matrix_vector_product_omp(double *a, double *b, double *c, int m, int n){
     }
 }
 
+double find_norm(double* vec){
+    double norm = 0;
+        double sumloc = 0.0;
+        #pragma omp for schedule(static)
+        for (int i = 0; i < N; i++){
+            norm += pow(vec[i], 2);
+        }
+        #pragma omp atomic
+        norm += sumloc;
+    return sqrt(norm);
+}
+
 double* solution_without_for(double* A, double* b){
     double *x = new double[N];
-    // #pragma omp parallel num_threads(nt)
-    // {
-    //     int nthreads = omp_get_num_threads();
-    //     int threadid = omp_get_thread_num();
-    //     int items_per_thread = N / nthreads;
-    //     int lb = threadid * items_per_thread;
-    //     int ub = (threadid == nthreads - 1) ? (N - 1) : (lb + items_per_thread - 1);
-    //     for (int i = lb; i <= ub; i++){
-        for (;;){
+
+        double norm_b = find_norm(b);
+        double norm_sub;
+        double *sub;
+        sub=new double[N];
+        while(true){
+
             double *c;
             c=new double[N];
+
             matrix_vector_product_omp(A,x,c,N,N);
-            for(int i=0; i<N;i++)
-                c[i]=(c[i] - b[i]) * variable;
-                
+
+            #pragma omp for schedule(static)
             for(int i=0; i<N;i++){
-                #pragma omp atomic
-                x[i]-=c[i];
+                sub[i]=c[i] - b[i];
             }
-            double *check=new double[N];
-            matrix_vector_product_omp(A,x,check,N,N);
-            double shit=0;
-            for(int i=0;i<N;i++){
-                shit+=(pow((check[i]-b[i]),2));
-            }
-            shit = sqrt(shit);
-            double help;
-            help=0;
-            for(int i=0;i<N;i++)
-                help+=(pow(b[i],2));
-            help=sqrt(help);
-            std::cout<<shit<<'/'<<help<<'='<<shit/help<<'\n';
-            if(shit/help<0.00001)
+
+            norm_sub=find_norm(sub);
+
+            // std::cout<<norm_sub<<'/'<<norm_b<<'='<<norm_sub/norm_b<<'\n';
+            // std::cout<<norm_sub<<'/'<<norm_b<<'='<<norm_sub/norm_b<<'\n';
+
+
+            if(norm_sub/norm_b<0.0000001)
                 break;
+            
+            #pragma omp for schedule(static)
+            for (int i = 0; i < N; i++)
+                sub[i] *= variable;
+
+            #pragma omp for schedule(static)
+            for (int i = 0; i < N; i++)
+                x[i] -= sub[i];
                 
         }
     // }
     return x;
 }
 
-double* solution_with_for(double* A, double* b){
-    for(int i = 0; i < N; i++){
-        double div = A[i*N+i];
-        for(int j = 0; j < N; j++){
-            A[i*N+j] /=div;
-        }
-        b[i] /= div;
-        for(int k = 0; k < N; k++){
-            if(k!=i){
-                double mult = A[k*N+i];
-                for (int l = 0; l < N; l++){
-                     A[k*N+l] -= mult * A[i*N+l];
-                }
-                b[k] -= mult * b[i];
-            }
-        }
-    }
-    return b;
-} 
-
 int main(){
     double *A;
     A = new double[N*N];
     for(int i = 0 ; i < N ; i++)
         for(int j = 0; j < N; j++){
-            A[i * N + j]=1;
             if(i == j)
                 A[i * N + j]=2;
+            else 
+                A[i * N + j]=1;
         }
     double *b;
     b = new double[N];   
@@ -104,17 +95,8 @@ int main(){
     double start = omp_get_wtime(); 
     x = solution_without_for(A,b);
     double end = omp_get_wtime(); 
-    printf("Elapsed time (parallel without for): %.6f sec.\n", end-start);
-    for(int i=0;i<N;i++)
-        printf("[%f],", x[i]);
-    
-    // printf("\n");
-    // start = omp_get_wtime(); 
-    // x = solution_with_for(A,b);
-    // end = omp_get_wtime();
-    // printf("Elapsed time (parallel with for): %.6f sec.\n", end-start);
-    // for(int i = 0; i < N; i++)
+    // for(int i=0;i<N;i++)
     //     printf("[%f],", x[i]);
-    // printf("\n");
+    printf("Elapsed time (parallel without for): %.6f sec.\n", end-start);
     return 0;
 }
